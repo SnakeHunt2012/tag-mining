5bbc535f511c04d8481a241c320603ac3cd28c16
SnakeHunt2012 <SnakeHunt2012@gmail.com>
2016-11-08 15:57:54 +0800

    Remove -x flag in run_nw_discov.sh

--- src/NewWordDiscov/run_nw_discov.sh
+++ src/NewWordDiscov/run_nw_discov.sh
@@ -3,9 +3,9 @@ if [ $# -gt 0 ]
 then
     DATE=$1
 fi
-sh -x  NewWordDiscov.sh  $DATE 1 0 2  
-sh -x create_word_co_count.sh $DATE
-sh -x create_word_entropy.sh $DATE
-sh -x create_word_pair_adhesion.sh $DATE
-sh -x create_word_pair_join_entropy.sh $DATE
+sh NewWordDiscov.sh  $DATE 1 0 2  
+sh create_word_co_count.sh $DATE
+sh create_word_entropy.sh $DATE
+sh create_word_pair_adhesion.sh $DATE
+sh create_word_pair_join_entropy.sh $DATE
 #sh -x run_srp_pv_click_check.sh $DATE
