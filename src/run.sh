229d9d253724525bc68d072f7dcd298df53163f7
SnakeHunt2012 <SnakeHunt2012@gmail.com>
2016-11-09 17:38:06 +0800

    Modify run.sh, modify make-reverse-index into streaming job.

--- src/run.sh
+++ src/run.sh
@@ -63,47 +63,58 @@ then
     exit ${err_proc}
 fi
 
-#shive -e "select query, pv from ${query_pv_table} where ds='${DATE}';" > query-pv.tsv
-#python segment.py query-pv.tsv seg-pv.tsv
-#
-#shadoop fs -test -e "${query_pv_dir}/ds=${DATE}"
-#if [ $? -eq 0 ]
-#then
-#    shadoop fs -rmr "${query_pv_dir}/ds=${DATE}"
-#fi
-#shadoop fs -mkdir "${query_pv_dir}/ds=${DATE}"
-#shadoop fs -put seg-pv.tsv "${query_pv_dir}/${DATE}"
-#
-#cd NewWordDiscov && bash run_nw_discov.sh "${DATE}" && cd ../
-#assert_status "Write into table nw_word_pair_join_entropy."
-#
-#shive -e "select word, co_word from nw_word_pair_join_entropy where ds='${DATE}' and (adhesion > 5000 and lentropy > 0.9) or (adhesion > 300000); "  | awk -F '\t' '{print $1 $2}' > double-query.tsv
-#assert_status "Dump double-query.tsv from table nw_word_pair_join_entropy"
-#
-#awk '{if (NF == 2) {print $1}}' seg-pv.tsv > single-query.tsv
-#assert_status "Extract single queyr from seg-pv.tsv."
-#
-#: > tag.tsv
-#cat single-query.tsv >> tag.tsv
-#cat double-query.tsv >> tag.tsv
-#
-#sort tag.tsv | uniq > tag-uniq.tsv
-#mv tag-uniq.tsv tag.tsv
-#
-#awk '{print $0 ":" NR}' tag.tsv > tag-dict.tsv
-#python fasttrie.py -f 'T(c*):(l)\n' < tag-dict.tsv > tag-tree.bin
-#assert_status "Build tag-tree.bin from tag-dict.tsv"
-#
+shive -e "select query, pv from ${query_pv_table} where ds='${DATE}';" > query-pv.tsv
+python segment.py query-pv.tsv seg-pv.tsv
+
+shadoop fs -test -e "${query_pv_dir}/ds=${DATE}"
+if [ $? -eq 0 ]
+then
+    shadoop fs -rmr "${query_pv_dir}/ds=${DATE}"
+fi
+shadoop fs -mkdir "${query_pv_dir}/ds=${DATE}"
+shadoop fs -put seg-pv.tsv "${query_pv_dir}/${DATE}"
+
+cd NewWordDiscov && bash run_nw_discov.sh "${DATE}" && cd ../
+assert_status "Write into table nw_word_pair_join_entropy."
+
+shive -e "select word, co_word from nw_word_pair_join_entropy where ds='${DATE}' and (adhesion > 5000 and lentropy > 0.9) or (adhesion > 300000); "  | awk -F '\t' '{print $1 $2}' > double-query.tsv
+assert_status "Dump double-query.tsv from table nw_word_pair_join_entropy"
+
+awk '{if (NF == 2) {print $1}}' seg-pv.tsv > single-query.tsv
+assert_status "Extract single queyr from seg-pv.tsv."
+
+: > tag.tsv
+cat single-query.tsv >> tag.tsv
+cat double-query.tsv >> tag.tsv
+
+sort tag.tsv | uniq > tag-uniq.tsv
+mv tag-uniq.tsv tag.tsv
+
+python format.py tag.tsv tag-dict.tsv
+python fasttrie.py -f 'T(c*):(l)\n' < tag-dict.tsv > tag-tree.bin
+assert_status "Build tag-tree.bin from tag-dict.tsv"
+
 shive -e "select split(btag, '\\\\|')[0], title from news_url_content_tag;" > category-title.tsv
 assert_status "Dump category-title.tsv from table news_url_content_tag."
 
-make make-reverse-index
-assert_status "Build make-reverse-index."
-condition="-e make-reverse-index"
-assert_condition "${condition}" ${LINENO}
+shadoop fs -test -e "/home/nlp/huangjingwen/data/category-title/ds=${DATE}"
+if [ $? -eq 0 ]
+then
+    shadoop fs -rmr "/home/nlp/huangjingwen/data/category-title/ds=${DATE}"
+fi
+shadoop fs -mkdir "/home/nlp/huangjingwen/data/category-title/ds=${DATE}"
+shadoop fs -put category-title.tsv "/home/nlp/huangjingwen/data/category-title/ds=${DATE}"
 
-./make-reverse-index tag-tree.bin category-title.tsv reverse-index.tsv
-assert_status "Make reverse-index.tsv."
+#make make-reverse-index
+#assert_status "Build make-reverse-index."
+#condition="-e make-reverse-index"
+#assert_condition "${condition}" ${LINENO}
+
+#./make-reverse-index tag-tree.bin category-title.tsv reverse-index.tsv
+#assert_status "Make reverse-index.tsv."
+
+bash make-reverse-index.sh "${DATE}"
+shadoop fs -cat "/home/nlp/huangjingwen/data/reverse-index/ds=${DATE}/part*" > reverse-index.tsv
 condition="-e reverse-index.tsv"
 assert_condition "${condition}" ${LINENO}
 
